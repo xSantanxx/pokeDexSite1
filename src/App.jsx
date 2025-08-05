ce3266b425f590664a22f52bcb0fffde3f331e59
xSantanxx <dmitripower@yahoo.com>
2025-08-05 17:28:50 -0400

    beginner layout

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [fade, setFade] = useState(false);
+
+  const triggerAnimation = () => {
+    setFade(true);
+  }
+
+  return (
+    <>
+    <div className='bg-gradient-to-r from-neutral-300 via-zinc-900 to-red-700
+    w-screen h-screen flex justify-center relative'>
+      <div onChange={fade ?   'animate-pulse' :   'none'} className='border-2 border-solid border-indigo-500 w-2xl h-1/3 absolute top-35
+      flex justify-center relative'>
+        <p className='absolute top-10 font-bold text-2xl hover:text-stone-50 cursor-pointer'>PokeDex Search</p>
+        <img src="pokeball.gif" alt="" className='absolute h-1/2 right-2
+        aspect-square rounded-full' />
+        <p className='absolute top-25 font-medium hover:text-stone-50 cursor-pointer'>
+          Pokemon Name
+        </p>
+        <input type="text" placeholder='Enter name' 
+        className='absolute top-35 border-solid border-2 rounded-lg w-xs bg-white'  />
+        <button type='button' onClick={triggerAnimation} className='absolute top-45 bg-neutral-900 hover:bg-red-500 text-white font-normal py-2 px-4 rounded
+        cursor-pointer'>
+          Click me</button>
+          <div className='absolute top-60 w-full border-2 border-solid
+          h-2/7 flex justify-center place-content-center'>
+            <button className='bg-neutral-900 hover:bg-red-500 text-white m-2 px-4 rounded'>
+              Normal</button>
+            <button className='bg-neutral-900 hover:bg-red-500 text-white m-2 px-4 rounded'>
+              Shiny Front</button>
+            <button className='bg-neutral-900 hover:bg-red-500 text-white m-2 px-4 rounded'>
+              Shiny Rear</button>
+            <button className='bg-neutral-900 hover:bg-red-500 text-white m-2 px-4 rounded'>
+              Default</button>
+            <button className='bg-neutral-900 hover:bg-red-500 text-white m-2 px-4 rounded'>
+              Clear</button>
+            <button className='bg-neutral-900 hover:bg-red-500 text-white m-2 px-4 rounded'>
+              Audio</button>
+          </div>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
